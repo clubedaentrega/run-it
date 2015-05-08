7a4521b373dd66fe0eecd07203e0db7fbab99eb1
Guilherme Souza <sitegui@sitegui.com.br>
2015-05-08 02:35:29 -0300

    New fns paramenter syntax

--- index.js
+++ index.js
@@ -8,24 +8,59 @@ var Bind = require('./lib/Bind')
 module.exports = function () {
 	/**
 	 * Two uses:
-	 * * run(fn) -> Bind
-	 * * run(fn, data, done)
+	 * * run(fns) -> Bind
+	 * * run(fns, data, done)
 	 * @param {Function|Array<Function>} fns
 	 * @param {...*} [data]
 	 * @param {Function} [done]
 	 * @returns {?Bind}
 	 */
 	var run = function (fns) {
-		var len = arguments.length
-		if (len === 1) {
-			// run(fn)
-			return new Bind(fns, run)
+		var isArr = Array.isArray,
+			preFns = [],
+			postFns = [],
+			targetFn
+
+		if (!isArr(fns)) {
+			fns = [fns]
 		}
 
-		var bind = new Bind(fns, run)
+		// Prepare functions
+		var hasArray = fns.some(isArr)
+		if (fns.length === 3 && isArr(fns[0]) && !isArr(fns[1]) && isArr(fns[2])) {
+			// run([preFns, targetFn, postFns])
+			preFns = fns[0]
+			targetFn = fns[1]
+			postFns = fns[2]
+		} else if (fns.length === 2 && isArr(fns[0]) && !isArr(fns[1])) {
+			// run([preFns, targetFn])
+			preFns = fns[0]
+			targetFn = fns[1]
+		} else if (fns.length === 2 && !isArr(fns[0]) && isArr(fns[1])) {
+			// run([targetFn, postFns])
+			targetFn = fns[0]
+			postFns = fns[1]
+		} else if (fns.length === 1 && !isArr(fns[0])) {
+			// run([targetFn])
+			targetFn = fns[0]
+		} else if (!hasArray) {
+			// run([...preFns, targetFn])
+			preFns = fns.slice(0, fns.length - 1)
+			targetFn = fns[fns.length - 1]
+		} else {
+			throw new Error('Invalid functions argument')
+		}
+
+		var len = arguments.length,
+			bind = new Bind(preFns, targetFn, postFns, run)
+		if (len === 1) {
+			// run(fns)
+			return bind
+		}
+
 		bind.exec.apply(bind, [].slice.call(arguments, 1))
 	}
-	
+
 	/** @member {string} */
 	run.basePath = ''
 
